965b90513e6bb70b01c8e84f49a623c736cd837a
VurimiPoojitha <vurimipoojitha@gmail.com>
2025-10-25 14:31:41 +0530

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,18 @@
+package com.spring;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class StudentInfo {
+	
+	private int id;
+	
+	private String name;
+	
+	private int age;
+
+}
